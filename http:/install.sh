e18ef5c33b08a5eed439580b56f69b84bf5a8f81
Rizki irfan <98686896+rizkiwibu@users.noreply.github.com>
2023-08-05 22:26:21 +0700

    Delete http: directory

--- http:/install.sh
+++
@@ -1,9 +0,0 @@
-bash
-#!/bin/bash
-
-# Memeriksa apakah variabel FFMPEG_INSTALL_METHOD ada dan nilainya adalah "prebuilt"
-if [ "$FFMPEG_INSTALL_METHOD" = "prebuilt" ]; then
-  # Menginstall ffmpeg menggunakan prebuilt binaries
-  apt-get update
-  apt-get install -y ffmpeg
-fi
